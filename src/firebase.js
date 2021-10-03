cac63d0dbfaf3f14dae5f7b28fbcf77aee63f5d8
pratiksha kale <pratikshakale909@gmail.com>
2021-10-03 16:17:57 +0530

    based project setting done

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgkKSEGwGCbA7l8g1F5-g1BUFPO_3MYZk",
+  authDomain: "whatsapp-react-clone-3e299.firebaseapp.com",
+  projectId: "whatsapp-react-clone-3e299",
+  storageBucket: "whatsapp-react-clone-3e299.appspot.com",
+  messagingSenderId: "334247636971",
+  appId: "1:334247636971:web:aaefb31bc16e3d0e10b3ae"
+};
